17851cf8df564248743f7ead3025f94726ce9d80
Junsang <kimnora070623@gmail.com>
2024-01-27 17:55:54 +0900

    레벨

--- Assets/Scripts/Bouns.cs
+++ Assets/Scripts/Bouns.cs
@@ -8,6 +8,7 @@ public class Bouns : MonoBehaviour
     private Rigidbody2D rb;
     public float minY = -5.5f;
     public float maxVelocity = 15f;
+    public int Hp = 5;
 
     private void Awake()
     {
@@ -18,6 +19,7 @@ public class Bouns : MonoBehaviour
     private void Update()
     {
         if(transform.position.y < minY){
+            Hp -= 1;
             transform.position = Vector3.zero;
             rb.velocity = Vector3.zero;
         }
--- Assets/Scripts/LevelGenerate.cs
+++ Assets/Scripts/LevelGenerate.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LevelGenerate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2Int size;
+    public Vector2 offSet;
+    public GameObject brickPrefab;
+    public Gradient gradient;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        for(int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                GameObject newBrick = Instantiate(brickPrefab, transform);
+                newBrick.transform.position = transform.position + 
+                                               new Vector3((float)((size.x-1)*.5f-i) * offSet.x, j* offSet.y, 0);
+                newBrick.GetComponent<SpriteRenderer>().color = gradient.Evaluate((float)j / (size.y - 1));
+            }
+        }
     }
 }
